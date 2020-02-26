3865f8f13bb867d2027fe159c60d9e0c8f7cf44d
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2020-02-25 19:28:27 -0700

    adding filed and compnents

---
+++ employee-directory/src/components/Navbar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+    return (
+        <nav class="navbar navbar-light bg-light">
+            <a class="navbar-brand">Navbar</a>
+        </nav>
+    );
+}
+        
+export default Navbar;
