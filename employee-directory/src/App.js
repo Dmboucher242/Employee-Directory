2307379f9d787de5ae4bffcef555ae91b1933f01
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2020-02-25 21:12:19 -0700

    adding a table header folder and files

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import Search from "./components/Search";
+// import TableHeader from "./components/TableHeader";
 
 function App() {
   return (
     <div>
       <Header />
       <Search />
+      {/* <TableHeader /> */}
     </div>
   );
 }
