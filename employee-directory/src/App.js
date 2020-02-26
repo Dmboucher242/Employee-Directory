50ed3820031e5c956852dbc36f69367fa04a60cd
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2020-02-25 20:38:53 -0700

    ..

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-
-
+import Header from "./components/Header";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Header />
+      <Search />
     </div>
   );
 }
 
 export default App;
+ 
---
+++ employee-directory/src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+function Card() {
+    return (
+        <>
+            <div class="card">
+                <div class="card-body">
+                    <h2>Employee Directory</h2>  
+                    <p>Search employee by name to filter results</p>             
+                 </div>
+            </div>
+            <div class="line"></div>
+        </>
+    );
+}
+
+export default Card;
