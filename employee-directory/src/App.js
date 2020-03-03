1ea0c211bad3da71612b4048dd5aa66b8c9094a4
Dmboucher242 <58062008+Dmboucher242@users.noreply.github.com>
2020-03-02 19:36:35 -0700

    adding a styles folder instead of doing the styles within the components

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
+import Main from "./components/Main";
+import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
-import Search from "./components/Search";
-// import TableHeader from "./components/TableHeader";
+
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Search />
-      {/* <TableHeader /> */}
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
     </div>
   );
 }
 
 export default App;
- 
